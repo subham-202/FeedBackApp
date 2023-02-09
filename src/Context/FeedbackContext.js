ca64d0086679a550383a6915bea34e564023c4a2
subham-202 <shubham.deep.202@gmail.com>
2023-02-09 12:48:51 +0530

    Context_Api Updated

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -1,83 +1,98 @@
-import { createContext,useState,useEffect } from "react";
-// import { v4 as uuidv4 } from 'uuid'
-const FeedbackContext=createContext();
-export const FeedbackProvider=({children})=>{
-    const[isLoading,setIsLoading]=useState(true);
-    const [feedback,setFeedback]=useState([
-        // {
-        //     id:1,
-        //     text:'This Feedback item 1',
-        //     rating:9
-        // },
-        // {
-        //     id: 2,
-        //     text: 'This Feedback item 2',
-        //     rating: 7 
-        // }, {
-        //     id: 3,
-        //     text: 'This Feedback item 3',
-        //     rating: 8
-        // }
-    ])
-    const[feedbackEdit,setFeedbackEdit]=useState({
-        item:{},
-        edit: false
+import { createContext, useState, useEffect } from 'react'
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false,
     })
-    useEffect(()=>{
-        fetchFeedback();
-    },[])
-    //Fetch Feedback
-    const fetchFeedback= async()=>{
-    const response = await fetch(`https://feedbackuiserver.onrender.com/feedback?_sort=id&_order=desc`);
-    const data =await response.json();
-    setFeedback(data);
-    setIsLoading(false);
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    // Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`https://feedbackuiserver.onrender.com/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
     }
-    //Update Feedback
-    const updateFeedback = async (id,updItem)=>{
-        // console.log(id,updItem)
-        const response = await fetch(`/feedback/${id}`,{
-        method:'PUT',
-        headers:{
-            "Content-Type":"application/json",
-        }
+
+    // Add feedback
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('https://feedbackuiserver.onrender.com/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
         })
-        const data =await response.json()
-        setFeedback(
-        feedback.map((item)=>(item.id===id?{...item,...data}:item)))
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback])
+    }
+
+    // Delete feedback
+    const deleteFeedback = async (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`https://feedbackuiserver.onrender.com/feedback/${id}`, { method: 'DELETE' })
+
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
-    //Set Items to be updated
-    const editFeedback =(item)=>{
+    }
+
+    // Update feedback item
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`https://feedbackuiserver.onrender.com/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem),
+        })
+
+        const data = await response.json()
+
+        // NOTE: no need to spread data and item
+        setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+
+        // FIX: this fixes being able to add a feedback after editing
+        // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+        setFeedbackEdit({
+            item: {},
+            edit: false,
+        })
+    }
+
+    // Set item to be updated
+    const editFeedback = (item) => {
         setFeedbackEdit({
             item,
-            edit:true,
+            edit: true,
         })
     }
-    // Delete Feedback
-    const deleteFeedback =async (id) => {
-        // console.log(`App`,id);
-        if (window.confirm("Are You Sure You want To Delete this Item?")) {
-            await fetch(`https://feedbackuiserver.onrender.com/feedback/${id}`,{method:'DELETE'})
-            setFeedback(feedback.filter((item) => item.id !== id));
-        }
-    }
-    // Add Feeedback
-    const addFeedback =async (newFeedback) => {
-        // newFeedback.id = uuidv4();
-        const response = await fetch('https://feedbackuiserver.onrender.com/feedback',{
-        method:'POST',
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(newFeedback),
-    })
-    const data =await response.json();
-    setFeedback([data, ...feedback])
-    }
-return (
-    <FeedbackContext.Provider value={{ feedback, feedbackEdit, isLoading, editFeedback, deleteFeedback, addFeedback, updateFeedback }}>
-    {children}
-    </FeedbackContext.Provider>
-)
+
+    return (
+        <FeedbackContext.Provider
+            value={{
+                feedback,
+                feedbackEdit,
+                isLoading,
+                deleteFeedback,
+                addFeedback,
+                editFeedback,
+                updateFeedback,
+            }}
+        >
+            {children}
+        </FeedbackContext.Provider>
+    )
 }
-export default FeedbackContext;
+
+export default FeedbackContext
